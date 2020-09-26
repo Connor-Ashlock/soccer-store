e9d007bbc3ebd1289deb220028f42a4c4cfbdc05
connor-ashlock <connor9804@yahoo.com>
2020-09-26 00:53:03 +0000

    added styling to header and added the header component to be rendered in the app component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="row">
-      <i className="fas fa-dollar-sign"></i>
-      <h1>Wicked Sales</h1>
+    <header className="row align-items-center">
+      <h2 className="ml-4 mb-0"><i className="mr-2 fas fa-dollar-sign"></i>Wicked Sales</h2>
     </header>
   );
 }
