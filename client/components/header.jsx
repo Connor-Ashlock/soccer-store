77d165024e0b2af919b96cde3b198ad1f5206c63
connor-ashlock <connor9804@yahoo.com>
2020-09-26 00:18:20 +0000

    added header file and a header component

---
+++ client/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="row">
+      <i className="fas fa-dollar-sign"></i>
+      <h1>Wicked Sales</h1>
+    </header>
+  );
+}
+
+export default Header;
