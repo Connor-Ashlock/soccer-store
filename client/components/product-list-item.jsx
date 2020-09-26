b1c5026c34d78187c660355977ccfeab93386dc3
connor-ashlock <connor9804@yahoo.com>
2020-09-26 01:13:22 +0000

    added product-list-item file and list item component

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProductListItem() {
+  return (
+    <div className="card">
+      <img src="..." alt="..." className="card-img-top"></img>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductListItem;
