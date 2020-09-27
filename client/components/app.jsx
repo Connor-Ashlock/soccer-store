49d2d1c9d6dd28abee023d8586c1fbad9c1db0a0
connor-ashlock <connor9804@yahoo.com>
2020-09-27 21:04:20 +0000

    added setView method to app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,6 +3,15 @@ import Header from './header';
 import ProductList from './product-list';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { view: { name: 'catalog', params: {} } };
+  }
+
+  setView(name, params) {
+    this.setState({ view: { name: name, params: params } });
+  }
+
   render() {
     return (
       <>
