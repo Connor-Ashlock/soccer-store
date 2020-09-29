0c9a27174d731755b1a03ce45f9b575b373b3e58
connor-ashlock <connor9804@yahoo.com>
2020-09-29 19:09:14 +0000

    changed map method to forEach method in the calculate total function

--- client/components/calculate-total.js
+++ client/components/calculate-total.js
@@ -2,7 +2,7 @@ import formatPrice from './format-price';
 
 function calculateTotal(array) {
   let total = 0;
-  array.map(item => {
+  array.forEach(item => {
     total += item.price;
   });
   return formatPrice(total);
