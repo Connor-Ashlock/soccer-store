2d8c26f30e27da9c3ee08f2584d0a60dbfad1753
connor-ashlock <connor9804@yahoo.com>
2020-09-29 00:56:19 +0000

    added CartSummary component and passed it to the render method of the app component

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -6,13 +6,13 @@ function CartSummaryItem(props) {
     <div className="card mb-3">
       <div className="row no-gutters">
         <div className="col-sm-4">
-          <img src={props.item.image} alt={props.item.name} className="card-img" />
+          <img src={props.item.image} alt={props.item.name} className="card-img cart-height" />
         </div>
         <div className="col-sm-8">
           <div className="card-body">
             <h5 className="card-title">{props.item.name}</h5>
             <p className="card-text text-muted">{formatPrice(props.item.price)}</p>
-            <p className="card-text">(props.item.shortDescription)</p>
+            <p className="card-text">{props.item.shortDescription}</p>
           </div>
         </div>
       </div>
